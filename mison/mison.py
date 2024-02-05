01f09d1be6bcd069d7523afb10884ab8f7af1ee2
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-02-05 16:19:32 +0200

    add: parametrize function

--- mison/mison.py
+++ mison/mison.py
@@ -1,10 +1,17 @@
+import datetime
+import argparse
+
 from pydriller import Repository, Commit, ModifiedFile
 import pandas as pd
 
 
-def mine_commits():
+def mine_commits(repo, branch, output=None):
+    if output is None:
+        output = f"mison_commits_mined_{datetime.datetime.now().isoformat()}.csv"
+
     data = []
-    for commit in Repository("/Users/abakhtin22/Documents/train-ticket", only_in_branch='master').traverse_commits():
+
+    for commit in Repository(repo, only_in_branch=branch).traverse_commits():
         for file in commit.modified_files:
             data.append([commit.hash, commit.author.name, commit.author.email, commit.committer.name,
                          commit.committer.email, commit.committer_date, file.added_lines, file.deleted_lines,
@@ -13,8 +20,17 @@ def mine_commits():
     data = pd.DataFrame(data, columns=['commit_hash', 'author_name', 'author_email', 'committer_name', 'committer_email',
                                        'commit_date', 'additions', 'deletions', 'filename'])
     print(data)
-    data.to_csv('2024.csv', index=False)
+    data.to_csv(output, index=False)
 
 
 if __name__ == '__main__':
-    mine_commits()
+    parser = argparse.ArgumentParser(description='MiSON - MicroService Organisational Network miner')
+
+    # Add the positional arguments
+    parser.add_argument('--branch', type=str, required=True, help='Name of the branch to mine')
+    parser.add_argument('--repo', type=str, required=True, help='Path to the repository (local path or URL)')
+    parser.add_argument('--commit_table', type=str, required=False, help='Output path for the csv table of mined commits')
+
+    # Parse the arguments
+    args = parser.parse_args()
+    mine_commits(repo=args.repo, branch=args.branch, output=args.commit_table)
